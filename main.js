d8da64a0476d9daffe17a961ff0a7e977ebc89d9
Stephen Schools <stephen.schools>
2017-08-09 11:42:30 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// 1. First select and store the elements you'll be working with
+// 2. Create your `submit` event for getting the user's search term
+// 3. Create your `fetch` request that is called after a submission
+// 4. Create a way to append the fetch results to your page
+// 5. Create a way to listen for a click that will play the song in the audio play
+let musicPlayer = document.querySelecor(".music-player");
+let searchForm = document.querySelecor(".search-form");
+let container = document.querySelecor(".results");
